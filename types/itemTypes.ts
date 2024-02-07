233ee5993cadf61df7ef19a4b4517c8abfabfd65
hyenaManager <zodiacjelly9@gmail.com>
2024-02-07 22:17:36 +0630

    add types

---
+++ types/itemTypes.ts
@@ -0,0 +1,14 @@
+
+export type NormalItem =  {
+    id:string,
+    name:string,
+    type:string,
+    madeIn: string,
+    image: string,
+    price: number,
+    left: number,
+    total:number, 
+    solded: number,
+    discountPercent:number,
+    description: string,
+  }
