b3a39357d96a0882b825c493f7e9fe4a1e3b8af6
hyenaManager <zodiacjelly9@gmail.com>
2024-05-01 21:06:45 +0800

    add userstate manager

---
+++ components/warpers/userStateManager.tsx
@@ -0,0 +1,30 @@
+"use client";
+
+import Cookies from "js-cookie";
+
+import axios from "axios";
+import { useEffect } from "react";
+import { useUser } from "@/app/store";
+
+const getUser = async (id: string) => {
+  const response = await axios.get(
+    `http://localhost:3001/users/getUserById?id=${id}`
+  );
+  if (response.status === 201) {
+    return response.data;
+  } else {
+    return alert("errror in fetching user:)");
+  }
+};
+
+export default function UserStateManager() {
+  const setUser = useUser((state) => state.setUser);
+  useEffect(() => {
+    const get_user = async () => {
+      const user = await getUser(Cookies.get("userToken") as string);
+      return setUser(user);
+    };
+    get_user();
+  }, []);
+  return null;
+}
